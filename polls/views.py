6c74450e9c59ae9ab290e266e53a1bf76d1e1752
minghui001 <41600645+minghui001@users.noreply.github.com>
2019-06-04 18:39:00 +0800

    完成第7节

--- polls/views.py
+++ polls/views.py
@@ -1,32 +1,33 @@
-from django.shortcuts import render,get_object_or_404
-from .models import Question,Choice
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-# Create your views here.
+from django.views import generic
+from django.utils import timezone
+from .models import Choice, Question
 
-# 首页，显示投票列表
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
 
-#一个路由测试页面
-def hello(request):
-    return HttpResponse('hello worlde!')
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-# 投票详情，路由传递一个数字过来，在这里判断数字（问题的id）是否存在，并作出动作
-# get_object_or_404会读取数据库Question，查找id，正确则返回object，错误则返回404页
-# 之后的返回代码就不会被执行了，例如：return HttpResponse('this is test')，实际测试只返回404
-# 页面中有提交的post，请求页面为id+vote，post内容为choice +安全码（防止跨站）
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    # return HttpResponse('this is test')
-    return render(request, 'polls/detail.html', {'question': question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
-#投票结果，路由为一个数字+results，其中数字为问题的id
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 # 投票，路由为id+vote，
 # 提交的post choice=id
